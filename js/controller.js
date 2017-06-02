cf7c125a7a725a5a8fe61a391c6f348e1c2004c5
chegior <chegiorca@hotmail.com>
2017-06-01 20:09:06 -0600

    Project done BEFORE BLACK DIAMOND

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,19 @@
 angular.module('myApp').controller('myCtrl',function($scope,mySvc){
-  $scope.print = mySvc.test;
+  $scope.quotes = mySvc.getQuotes();
+
+  $scope.deleteQuote =  function(textToDelete){
+    mySvc.removeData(textToDelete);
+  }
+
+  $scope.addQuote = function(){
+    var newQuote = {
+      text:$scope.newQuoteText,
+      author:$scope.newQuoteAuthor
+    }
+    if(mySvc.addData(newQuote)){
+      $scope.newQuoteText='';
+      $scope.newQuoteAuthor=''
+    }
+  }
 
 });
